d99c92dcd77d31da7829e3f8c3073dc7310ad1e6
szeltamas <tamas.szel.2001@gmail.com>
2024-09-20 17:17:37 +0200

    Commit 4 - Browse Screen Done

--- lib/screens/Browsescreen.dart
+++ lib/screens/Browsescreen.dart
@@ -6,11 +6,11 @@ class BrowseScreen extends StatelessWidget {
   const BrowseScreen({super.key});
 
   Future<List<String>> _loadImagePaths() async {
-    // This assumes that the assets are declared in pubspec.yaml
+    // Load the AssetManifest.json to get the list of assets
     final manifestContent = await rootBundle.loadString('AssetManifest.json');
     final Map<String, dynamic> manifestMap = json.decode(manifestContent);
 
-    // Filter out the images in the plant_images directory
+    // Filter the assets to only include those in the plant_images directory
     final imagePaths = manifestMap.keys
         .where((String key) => key.startsWith('images/plant_images/'))
         .toList();
@@ -21,72 +21,118 @@ class BrowseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Browse Plants'),
-        backgroundColor: Colors.green,
-      ),
-      body: FutureBuilder<List<String>>(
-        future: _loadImagePaths(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error loading images'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No images found'));
-          } else {
-            final imagePaths = snapshot.data!;
-            return GridView.builder(
-              padding: EdgeInsets.all(10),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/mobilebackground.jpg"), // Background image
+                fit: BoxFit.cover, // Cover the entire screen
               ),
-              itemCount: imagePaths.length,
-              itemBuilder: (context, index) {
-                final imagePath = imagePaths[index];
-                final fileName = imagePath.split('/').last.split('.').first;
-
-                return GestureDetector(
-                  onTap: () {
-                    // Handle image tap here if needed
-                  },
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: Image.asset(
-                          imagePath,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        fileName,
-                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            );
-          }
-        },
-      ),
-      bottomNavigationBar: Container(
-        height: 80, // Height of the dashboard-like footer
-        color: Colors.lightGreen, // Light green color for the footer
-        child: Row(
-          children: [
-            IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.white, size: 44), // Back icon
-              onPressed: () {
-                Navigator.pop(context); // Go back to MainPage
-              },
             ),
-            Spacer(), // Pushes the icon to the left
-          ],
-        ),
+          ),
+          Column(
+            children: [
+              AppBar(
+                title: Text('Browse Plants'),
+                backgroundColor: Colors.lightGreen,
+              ),
+              Expanded(
+                child: FutureBuilder<List<String>>(
+                  future: _loadImagePaths(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Error loading images'));
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return Center(child: Text('No images found'));
+                    } else {
+                      final imagePaths = snapshot.data!;
+                      return GridView.builder(
+                        padding: EdgeInsets.all(10),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                          childAspectRatio: 1, // Make containers square
+                        ),
+                        itemCount: imagePaths.length,
+                        itemBuilder: (context, index) {
+                          final imagePath = imagePaths[index];
+                          final fileName = imagePath.split('/').last.split('.').first;
+
+                          return GestureDetector(
+                            onTap: () {
+                              // Handle image tap here if needed
+                            },
+                            child: Container(
+                              width: 150, // Fixed width for uniformity
+                              height: 180, // Adjusted height for better fit
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.85), // Background color
+                                borderRadius: BorderRadius.circular(25.0), // Rounded corners
+                              ),
+                              child: Column(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.vertical(top: Radius.circular(25.0)),
+                                    child: Image.asset(
+                                      imagePath,
+                                      fit: BoxFit.cover, // Ensure image fills the container
+                                      width: double.infinity,
+                                      height: 130, // Fixed height for the image
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 50, // Adjusted height for the text
+                                    padding: EdgeInsets.symmetric(vertical: 5.0), // Reduced padding
+                                    child: Center(
+                                      child: Text(
+                                        fileName,
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                        overflow: TextOverflow.ellipsis, // Prevent overflow
+                                        maxLines: 1, // Limit to one line
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 80,
+              color: Colors.lightGreen,
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.white, size: 44),
+                    onPressed: () {
+                      Navigator.pop(context); // Go back to MainPage
+                    },
+                  ),
+                  Spacer(),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
