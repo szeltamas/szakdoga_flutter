a743393561022dad9588e8cee296c81ff7fc7373
szeltamas <tamas.szel.2001@gmail.com>
2024-09-22 20:38:07 +0200

    NewTestScreen Test

--- lib/screens/Identifyscreen.dart
+++ lib/screens/Identifyscreen.dart
@@ -1,141 +1,124 @@
-import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:image/image.dart' as img;
-import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:image_picker/image_picker.dart'; // Import the image picker package
+import '../custom_widgets/CustomButton.dart';
+import 'dart:io'; // Needed to handle files
+import 'NewTestScreen.dart'; // Import the new test screen
 
 class IdentifyScreen extends StatefulWidget {
+  const IdentifyScreen({super.key});
+
   @override
   _IdentifyScreenState createState() => _IdentifyScreenState();
 }
 
 class _IdentifyScreenState extends State<IdentifyScreen> {
-  File? _image;
-  String _classificationResult = "No result";
-  late Interpreter _interpreter;
+  final ImagePicker _picker = ImagePicker(); // Initialize the image picker
+  File? _selectedImage; // To hold the selected image
 
-  final List<String> _labels = [
-    "alpine sea holly", "anthurium", "artichoke", "azalea", "ball moss",
-    "balloon flower", "barbeton daisy", "bearded iris", "bee balm", "bird of paradise",
-    "bishop of llandaff", "black-eyed susan", "blackberry lily", "blanket flower",
-    "bolero deep blue", "bougainvillea", "bromelia", "buttercup", "californian poppy",
-    "camellia", "canna lily", "canterbury bells", "cape flower", "carnation",
-    "cautleya spicata", "clematis", "colt's foot", "columbine", "common dandelion",
-    "corn poppy", "cyclamen", "daffodil", "desert-rose", "english marigold",
-    "fire lily", "foxglove", "frangipani", "fritillary", "garden phlox", "gaura",
-    "gazania", "geranium", "giant white arum lily", "globe thistle", "globe-flower",
-    "grape hyacinth", "great masterwort", "hard-leaved pocket orchid", "hibiscus",
-    "hippeastrum", "japanese anemone", "king protea", "lenten rose", "lotus lotus",
-    "love in the mist", "magnolia", "mallow", "marigold", "mexican aster",
-    "mexican petunia", "monkshood", "moon orchid", "morning glory", "orange dahlia",
-    "osteospermum", "oxeye daisy", "passion flower", "pelargonium", "peruvian lily",
-    "petunia", "pincushion flower", "pink primrose", "pink-yellow dahlia", "poinsettia",
-    "primula", "prince of wales feathers", "purple coneflower", "red ginger", "rose",
-    "ruby-lipped cattleya", "siam tulip", "silverbush", "snapdragon", "spear thistle",
-    "spring crocus", "stemless gentian", "sunflower", "sweet pea", "sweet william",
-    "sword lily", "thorn apple", "tiger lily", "toad lily", "tree mallow", "tree poppy",
-    "trumpet creeper", "wallflower", "water lily", "watercress", "wild pansy",
-    "windflower", "yellow iris"
-  ];
+  // Method to open camera
+  Future<void> _pickImageFromCamera() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
 
-  @override
-  void initState() {
-    super.initState();
-    loadModel();
-  }
-
-  Future<void> loadModel() async {
-    _interpreter = await Interpreter.fromAsset("custom_files/model.tflite");
-  }
-
-  Future<void> pickImage(ImageSource source) async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.getImage(source: source);
-
-    if (pickedFile != null) {
+    if (image != null) {
       setState(() {
-        _image = File(pickedFile.path);
-      });
-      classifyImage(_image!);
-    }
-  }
-
-  Future<void> classifyImage(File image) async {
-    // Preprocess the image similar to Python preprocessing
-    final img.Image? imgRaw = img.decodeImage(image.readAsBytesSync());
-
-    if (imgRaw != null) {
-      final img.Image imgResized = img.copyResize(imgRaw, width: 128, height: 128);
-      Float32List inputData = _imageToFloat32List(imgResized);
-
-      // Create a buffer for output
-      var output = List.filled(1 * _labels.length, 0).reshape([1, _labels.length]);
-
-      // Run the model
-      _interpreter.run(inputData, output);
-
-      // Process the results
-      int index = output[0].indexWhere((element) => element == output[0].reduce((a, b) => a > b ? a : b));
-      setState(() {
-        _classificationResult = _labels[index];
+        _selectedImage = File(image.path);
       });
     }
   }
 
-  Float32List _imageToFloat32List(img.Image image) {
-    var convertedBytes = Float32List(128 * 128 * 3);
+  // Method to open gallery
+  Future<void> _pickImageFromGallery() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
 
-    int pixelIndex = 0;
-    for (int i = 0; i < 128; i++) {
-      for (int j = 0; j < 128; j++) {
-        final pixel = image.getPixel(j, i);
-
-        convertedBytes[pixelIndex++] = img.getRed(pixel) / 255.0;
-        convertedBytes[pixelIndex++] = img.getGreen(pixel) / 255.0;
-        convertedBytes[pixelIndex++] = img.getBlue(pixel) / 255.0;
-      }
+    if (image != null) {
+      setState(() {
+        _selectedImage = File(image.path);
+      });
     }
-    return convertedBytes; // Return the single Float32List
   }
 
-  @override
-  void dispose() {
-    _interpreter.close();
-    super.dispose();
+  // Method to navigate to NewTestScreen
+  void _navigateToNewTestScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => NewTestScreen()), // Navigate to NewTestScreen
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Plant Identifier'),
+        title: Text('Identify Plant'),  // Title of the screen
         backgroundColor: Colors.lightGreen,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black, size: 28),
+          onPressed: () {
+            Navigator.pop(context); // Go back to MainPage
+          },
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          if (_image != null) ...[
-            Image.file(_image!),
-            SizedBox(height: 20),
-            Text(
-              'This plant is a: $_classificationResult',
-              style: TextStyle(fontSize: 24),
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/mobilebackground.jpg"),
+                fit: BoxFit.cover,
+              ),
             ),
-          ],
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              ElevatedButton(
-                onPressed: () => pickImage(ImageSource.camera),
-                child: Text('Take a Picture'),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // Camera Button
+                    CustomButton(
+                      text: "Snap a picture",
+                      icon: Icons.add_a_photo,
+                      onPressed: _pickImageFromCamera, // Open camera
+                    ),
+                    SizedBox(height: 30), // Space between buttons
+
+                    // Gallery Button
+                    CustomButton(
+                      text: "Select from local device",
+                      icon: Icons.folder,
+                      onPressed: _pickImageFromGallery, // Open gallery
+                    ),
+                    SizedBox(height: 30),
+
+                    // Navigate to NewTestScreen Button
+                    CustomButton(
+                      text: "Go to New Test Screen",
+                      icon: Icons.navigate_next,
+                      onPressed: _navigateToNewTestScreen, // Navigate to NewTestScreen
+                    ),
+                    SizedBox(height: 30),
+
+                    // Display the selected image
+                    if (_selectedImage != null)
+                      Image.file(
+                        _selectedImage!,
+                        width: 300,
+                        height: 300,
+                        fit: BoxFit.cover,
+                      ),
+                  ],
+                ),
               ),
-              ElevatedButton(
-                onPressed: () => pickImage(ImageSource.gallery),
-                child: Text('Choose from Gallery'),
-              ),
-            ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 60, // Height of the dashboard-like footer
+              color: Colors.lightGreen, // Light green color for the footer
+            ),
           ),
         ],
       ),
---
+++ lib/screens/NewTestScreen.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:image/image.dart' as img;
+
+class NewTestScreen extends StatefulWidget {
+  const NewTestScreen({super.key});
+
+  @override
+  _NewTestScreenState createState() => _NewTestScreenState();
+}
+
+class _NewTestScreenState extends State<NewTestScreen> {
+  File? _image;
+  String? _predictedLabel;
+  final String modelPath = "custom_files/model.tflite";
+  final String labelsPath = "custom_files/labels.json";
+  late Interpreter _interpreter;
+  List<String>? _labels;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadModel();
+    _loadLabels();
+  }
+
+  Future<void> _loadModel() async {
+    _interpreter = await Interpreter.fromAsset(modelPath);
+    debugPrint("Loadmodel ended");
+  }
+
+  Future<void> _loadLabels() async {
+    final jsonString = await rootBundle.loadString(labelsPath);
+    _labels = List<String>.from(json.decode(jsonString));
+    debugPrint("Loadlabels ended");
+  }
+
+  Future<void> _chooseImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+        _predictedLabel = null; // Reset prediction
+      });
+      await _predictImage(_image!);
+    }
+  }
+
+  Future<void> _predictImage(File image) async {
+    var inputImage = await _preprocessImage(image);
+    var output = List.generate(1, (b) => List<double>.filled(_labels!.length, 0.0));
+
+    // Run inference
+    _interpreter.run(inputImage, output);
+    // debugPrint(output.toString());
+
+    int predictedClass = output[0].indexOf(output[0].reduce((a, b) => a > b ? a : b));
+    debugPrint("Predicted Class" + predictedClass.toString());
+    debugPrint("Predicted Class Name" + " " + _labels![predictedClass]);
+    debugPrint(_labels![predictedClass] + " " + output[0][predictedClass].toString());
+    setState(() {
+      _predictedLabel = _labels![predictedClass];
+    });
+  }
+
+  Future<List<List<List<List<double>>>>> _preprocessImage(File image) async {
+    final inputSize = 128;
+    final imageBytes = await image.readAsBytes();
+    img.Image? decodedImage = img.decodeImage(imageBytes);
+
+    // Resize and normalize
+    img.Image resizedImage = img.copyResize(decodedImage!, width: inputSize, height: inputSize);
+
+    // Create a 4D list for model input
+    List<List<List<List<double>>>> input = List.generate(1, (b) => // Batch size = 1
+    List.generate(inputSize, (i) =>
+        List.generate(inputSize, (j) => [
+          img.getRed(resizedImage.getPixel(j, i)) / 255.0, // Normalize Red
+          img.getGreen(resizedImage.getPixel(j, i)) / 255.0, // Normalize Green
+          img.getBlue(resizedImage.getPixel(j, i)) / 255.0 // Normalize Blue
+        ])
+    )
+    );
+
+    return input; // Returns a 4D list suitable for the model input
+  }
+
+  @override
+  void dispose() {
+    _interpreter.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreen,
+        title: Row(
+          children: [
+            Icon(Icons.eco, color: Colors.black, size: 36),
+            SizedBox(width: 10),
+            Text('New Test Screen'),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/mobilebackground.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      onPressed: _chooseImage,
+                      child: Text('Choose Image'),
+                    ),
+                    if (_image != null)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: Image.file(
+                          _image!,
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    if (_predictedLabel != null)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: Text(
+                          'Predicted: $_predictedLabel',
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 80,
+              color: Colors.lightGreen,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
