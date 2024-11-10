6ec2cee88ed17d7d3b4d1a7c5236935589c6df0a
szeltamas <tamas.szel.2001@gmail.com>
2024-11-10 11:35:34 +0100

    Star icon and portrait mode

--- lib/custom_widgets/CustomFooter.dart
+++ lib/custom_widgets/CustomFooter.dart
@@ -5,16 +5,6 @@ import 'package:szakdoga/screens/FavoritesScreen.dart'; // Import the new Favori
 class FooterWidget extends StatelessWidget {
   const FooterWidget({super.key});
 
-  // Method to launch the URL
-  void _launchURL() async {
-    const url = 'https://github.com/szeltamas';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Positioned(
@@ -25,24 +15,13 @@ class FooterWidget extends StatelessWidget {
         height: 60,
         color: Colors.lightGreen,
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween, // Space between items
+          mainAxisAlignment: MainAxisAlignment.center, // Center the items
           children: [
-            // GitHub Icon on the left side
-            IconButton(
-              icon: const Icon(
-                Icons.open_in_new,
-                color: Colors.black,
-                size: 30, // Increase the size of the icon
-              ),
-              onPressed: _launchURL, // Launch the URL when pressed
-              tooltip: 'Go to GitHub',
-            ),
-            // Hollow Star Icon in the center
             IconButton(
               icon: const Icon(
                 Icons.star_border,
-                color: Colors.black, // Color of the hollow star icon
-                size: 30, // Increase the size of the icon
+                color: Colors.black,
+                size: 30,
               ),
               onPressed: () {
                 // Navigate to the FavoritesScreen when the icon is pressed
@@ -53,8 +32,6 @@ class FooterWidget extends StatelessWidget {
               },
               tooltip: 'Favorites',
             ),
-            // Placeholder for the right side if needed, or remove it if only two icons are used
-            const SizedBox(width: 30), // Adjust this or replace with another button if needed
           ],
         ),
       ),
